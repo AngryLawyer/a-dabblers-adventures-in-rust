32604465a2c15d7405d414daade45bb68ef9fe45
Tony Aldridge <zaragopha@hotmail.com>
2014-01-09 19:05:29 +0000

    Data types

---
+++ snippets/rust-5.rs
@@ -0,0 +1,26 @@
+fn main() {
+//---START
+    //We have structs, which are like their C equivalent.
+    struct llama {
+        hairiness: uint
+    };
+
+    let jose = llama{hairiness: 5};
+
+    //We also have tuples
+    let tuple = (5, "lol", 7);
+
+    //And a typed list-like structure called vectors
+    let my_list = ["first", "second", "third"];
+
+    //Finally, we have enums, which we'll take a closer look at later
+    enum Colors {
+        Red,
+        Blue,
+        Green
+    };
+
+    let myColor = Red;
+
+//---END
+}
