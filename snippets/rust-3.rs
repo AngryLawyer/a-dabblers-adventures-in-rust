f65fb0b38f9f97284e621284b700a21b0eb32c04
Tony Aldridge <tony@dabapps.com>
2014-01-09 14:02:45 +0000

    More snippets

--- snippets/rust-3.rs
+++ snippets/rust-3.rs
@@ -2,6 +2,8 @@ fn main() {
     let b = 5;
 //---START
     //As everything's an expression, we get some fun results
+    //If statements:
+
     let a = if (b > 0) {
         "Positive"
     } else {
@@ -9,7 +11,7 @@ fn main() {
     };
 
     //We also get pattern matching
-    match b {
+    let c = match b {
         3 | 5 => "Lol",
         _ => "Not lol"
     };
---
+++ snippets/rust-4.rs
@@ -0,0 +1,21 @@
+//----START
+//Functions must specify the types they use
+fn increment (a: uint) -> uint {
+    a + 1
+}
+
+//Functions are first class. Return the last value in them
+fn munge_triple((a, b, c): (uint, uint, uint), munger: |a: uint| -> uint) -> (uint, uint, uint) {
+    (munger(a), munger(b), munger(c))
+}
+
+fn main() {
+    let a = 1;
+    let b = increment(a);
+
+    let c = munge_triple((1, 2, 3), increment);
+    //There's also shorthand
+    let c = munge_triple((1, 2, 3), |a| a+1);
+
+}
+//---END
