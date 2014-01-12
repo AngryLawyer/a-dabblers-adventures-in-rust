328e6b689652df2a8d88c3e4dbee7b779c989d9d
Tony Aldridge <zaragopha@hotmail.com>
2014-01-12 15:14:47 +0000

    More sensible error catching

--- apply_snippets.py
+++ apply_snippets.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python
 
 def import_line(line):
-    name = line[3:]
+    (name, _separator, subgroup) = line[3:].partition(":")
+
     with open("snippets/{0}".format(name)) as snippet:
         snippet_content = snippet.read().splitlines()
         # We need to find the start and end slice points
-        start = snippet_content.index("//---START");
-        end = snippet_content.index("//---END");
+        try:
+            if subgroup:
+                start = snippet_content.index("//---START:{0}".format(subgroup));
+                end = snippet_content.index("//---END:{0}".format(subgroup));
+            else:
+                start = snippet_content.index("//---START");
+                end = snippet_content.index("//---END");
+        except ValueError:
+            raise Exception("Could not find snip tags for {0}".format(line[3:]))
         indented_content = ["    {0}".format(line) for line in snippet_content[start + 1:end]]
         return "    !rust\n{0}".format("\n".join(indented_content))
 
